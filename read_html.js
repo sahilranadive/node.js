75bb08bd06b85d7babe4cb1ea27587a32a045faf
sahilranadive <32863240+sahilranadive@users.noreply.github.com>
2018-06-26 11:49:04 +0530

    Add files via upload

--- read_html.js
+++ read_html.js
@@ -1,15 +1,22 @@
 var http = require('http');
 var fs = require('fs');
+var writeStream = fs.createWriteStream('output.txt');
+var v = fs.readFileSync('input.txt');
+writeStream.write(v.toString(),'UTF8');
+//writeStream.end();
+writeStream.on('error', function(err){
+console.log(err.stack);
+});
 
 http.createServer(function(req,res){
   fs.readFile('input.txt',function(err,data){
   //fs.readFile('new_html.html',function(err,data){
     res.writeHead(200,{'Content-Type':'text/html'});
     //res.write()
-    var v = fs.readFileSync('input.txt');
-    res.write(v.toString());
-    res.write("\t \t \t \t");
-    res.write(data);
+
+    res.write(data.toString());
+    //res.write("\t \t \t \t");
+  //  res.write(data);
     res.end();
   });
 }).listen(8080);
